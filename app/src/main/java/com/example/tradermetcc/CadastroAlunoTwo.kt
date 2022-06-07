7ec74fc5b8cc71735edd61950c36e8c62565f945
caiomachhado <caioamsul@gmail.com>
2022-06-07 17:24:10 -0300

    Design Save

--- app/src/main/java/com/example/tradermetcc/CadastroAlunoTwo.kt
+++ app/src/main/java/com/example/tradermetcc/CadastroAlunoTwo.kt
@@ -1,11 +1,27 @@
 package com.example.tradermetcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CadastroAlunoTwo : AppCompatActivity() {
+
+    private var btn_endCreateAccount : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_aluno_two)
+
+        initialise()
+
+    }
+
+    private fun initialise() {
+
+        btn_endCreateAccount = findViewById(R.id.btn_endCreateAccount)
+
+        btn_endCreateAccount?.setOnClickListener { startActivity(Intent(this@CadastroAlunoTwo, PDIStart::class.java)) }
+
     }
 }
