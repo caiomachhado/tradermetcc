a11f8ef32aa158ffdb67d7942af69500b48680f0
caiomachhado <caioamsul@gmail.com>
2022-06-07 22:00:59 -0300

    Design Save

--- app/src/main/java/com/example/tradermetcc/PDI_Nono.kt
+++ app/src/main/java/com/example/tradermetcc/PDI_Nono.kt
@@ -1,14 +1,27 @@
 package com.example.tradermetcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PDI_Nono : AppCompatActivity() {
 
-
+    private var next09 : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdi_nono)
+
+        initialise()
+
+    }
+
+    private fun initialise() {
+
+        next09 = findViewById(R.id.next09)
+
+        next09?.setOnClickListener { startActivity(Intent(this@PDI_Nono, PDI_Decimo::class.java)) }
+
     }
 }
