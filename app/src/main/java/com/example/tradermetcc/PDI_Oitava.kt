0fe14f3ee20505cb47dfa791cede539a034ea63b
caiomachhado <caioamsul@gmail.com>
2022-06-07 21:56:58 -0300

    Design Save

--- app/src/main/java/com/example/tradermetcc/PDI_Oitava.kt
+++ app/src/main/java/com/example/tradermetcc/PDI_Oitava.kt
@@ -1,11 +1,28 @@
 package com.example.tradermetcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PDI_Oitava : AppCompatActivity() {
+
+    private var next08 : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdi_oitava)
+
+        initialise()
+
     }
+
+    private fun initialise() {
+
+        next08 = findViewById(R.id.next08)
+
+        next08?.setOnClickListener { startActivity(Intent(this@PDI_Oitava, PDI_Nono::class.java)) }
+
+    }
+
 }
