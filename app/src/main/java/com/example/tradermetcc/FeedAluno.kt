92ae259cfbd5799c51c97255db9c104822ad735c
caiomachhado <caioamsul@gmail.com>
2022-06-08 18:58:12 -0300

    Design Save

---
+++ app/src/main/java/com/example/tradermetcc/FeedAluno.kt
@@ -0,0 +1,11 @@
+package com.example.tradermetcc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FeedAluno : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_feed_aluno)
+    }
+}
--- app/src/main/java/com/example/tradermetcc/InserirAulas.kt
+++ app/src/main/java/com/example/tradermetcc/InserirAulas.kt
@@ -1,11 +1,38 @@
 package com.example.tradermetcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
 
 class InserirAulas : AppCompatActivity() {
+
+    private var selectVideo : Button? = null
+
+    private val REQUEST_GALERIA = 20
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inserir_aulas)
+
+        initialise()
+
+        selectVideo?.setOnClickListener { abreGaleria() }
+
     }
+
+    private fun initialise() {
+        selectVideo = findViewById(R.id.selectVideo)
+
+
+    }
+    
+    private fun abreGaleria(){
+
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, REQUEST_GALERIA)
+
+    }
+
 }
