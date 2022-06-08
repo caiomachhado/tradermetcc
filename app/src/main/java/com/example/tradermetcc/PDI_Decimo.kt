2c1a78de855f10c6cbed49c7d9470c62e7bfcdac
caiomachhado <caioamsul@gmail.com>
2022-06-07 22:16:36 -0300

    Design Save

--- app/src/main/java/com/example/tradermetcc/PDI_Decimo.kt
+++ app/src/main/java/com/example/tradermetcc/PDI_Decimo.kt
@@ -1,11 +1,28 @@
 package com.example.tradermetcc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class PDI_Decimo : AppCompatActivity() {
+
+    private var next10 : Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pdi_decimo)
+
+        initialise()
+
     }
+
+    private fun initialise() {
+
+        next10 = findViewById(R.id.next10)
+
+        next10?.setOnClickListener { startActivity(Intent(this@PDI_Decimo, PDI_Ultimo::class.java)) }
+
+    }
+
 }
